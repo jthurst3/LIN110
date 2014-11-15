ca2368919acb9ac99805f6da3f4973e4c06c11c4
J. Hassler Thurston <jthurst3@u.rochester.edu>
2014-11-15 15:54:33 -0500

    add parse tree template

---
+++ parse_trees.py
@@ -0,0 +1,7 @@
+# parse_trees.py
+# Given a CFG and a sentence, general all possible parse trees for the sentence
+# Uses NLTK (Natural Language toolkit) and CYK algorithm
+# Jamie Alexander, Luisa Neves, Ben Ouattara, J. Hassler Thurston
+# November 15, 2014
+
+
