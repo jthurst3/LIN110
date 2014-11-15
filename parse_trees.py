325e7af6944fafc44595be9ca6961e3e059fc40a
J. Hassler Thurston <jthurst3@u.rochester.edu>
2014-11-15 15:55:54 -0500

    modify template slightly

--- parse_trees.py
+++ parse_trees.py
@@ -4,4 +4,14 @@
 # Jamie Alexander, Luisa Neves, Ben Ouattara, J. Hassler Thurston
 # November 15, 2014
 
+import nltk
+# from nltk.corpus import treebank
 
+
+def generate_parse_tree(sentence):
+    tokens = nltk.word_tokenize(sentence)
+    return tokens
+
+
+if __name__ == '__main__':
+    print generate_parse_tree("the boy walked")
