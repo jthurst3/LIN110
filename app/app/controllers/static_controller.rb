d5caad04081e4111b816acd24b8d5efc34bb6c02
benny <benny.ouattara@gmail.com>
2014-12-01 17:12:25 -0500

    progress on script exec

--- app/app/controllers/static_controller.rb
+++ app/app/controllers/static_controller.rb
@@ -1,5 +1,17 @@
 class StaticController < ApplicationController
   def home
-  	@log = Log.new
+  	a = {"1"=>"one", "2"=>"two"}
+  	@log = params
+  	text = params['text_to_parse']
+  	if text
+  		ret = %x("python parse_trees #{text} 3")
+  		render json:ret.to_json
+  	end
   end
+
+  def path
+  	a = {"1"=>"one", "2"=>"two"}
+  	redirect_to '/'
+  end
+
 end
--- app/config/routes.rb
+++ app/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-resources :logs 
 root 'static#home'
+post '/' => 'static#home'
+post '/path' => 'static#path'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
